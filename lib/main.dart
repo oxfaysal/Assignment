689e3e9e49b5224675c882b94f3360c9ccf22f9d
Faysal <0xfaysal64@gmail.com>
2026-02-04 19:22:41 +0600

    main: Assignment 1 Add

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
-import 'package:flutter/material.dart';
-
 void main() {
-  // runApp(const MyApp());
-  // git add
-  // git commit
-  // check git
-}
 
+  // Assignment: Working with Sets in Dart
+
+  Set<int> A = {1, 2, 3, 4, 5};
+  Set<int> B = {4, 5, 6, 7, 8};
+
+  for(int result in A.union(B)){
+    print(result);
+  }
+
+
+}
