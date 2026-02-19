f2bf7d669dd116b632f61dcd94ab155c2ae23324
Faysal <0xfaysal64@gmail.com>
2026-02-19 22:31:10 +0600

    Tasbih Counter App Complete

---
+++ lib/conts/app_text_style.dart
@@ -0,0 +1,35 @@
+import 'dart:ui';
+
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTextStyle {
+
+  static final titleBlack16 = GoogleFonts.playfairDisplay(
+    fontSize: 16,
+    color: Color(0xFF000000),
+    fontWeight: FontWeight.w700
+  );
+
+
+  static final textWhite20W700 = GoogleFonts.playfairDisplay(
+    fontSize: 20,
+    color: Color(0xFFFFFFFF),
+    fontWeight: FontWeight.w700
+  );
+
+  static final timerParpal16W700 = GoogleFonts.dmSans(
+    fontSize: 16,
+    color: Color(0xFF764CA5),
+    fontWeight: FontWeight.w700
+  );
+
+  static final counterWhite20W700 = GoogleFonts.dmSans(
+    fontSize: 20,
+    color: Color(0xFFFFFFFF),
+    fontWeight: FontWeight.w700
+  );
+
+
+
+
+}
