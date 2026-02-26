46ce0cff8674d24e3cf1bc350ee94bcb5337c579
Faysal <0xfaysal64@gmail.com>
2026-02-26 17:11:42 +0600

    Init Gallery App

--- lib/conts/app_text_style.dart
+++ lib/conts/app_text_style.dart
@@ -4,30 +4,12 @@ import 'package:google_fonts/google_fonts.dart';
 
 class AppTextStyle {
 
-  static final titleBlack16 = GoogleFonts.playfairDisplay(
-    fontSize: 16,
+  static final titleBlack36 = GoogleFonts.comfortaa(
+    fontSize: 36,
     color: Color(0xFF000000),
-    fontWeight: FontWeight.w700
+    fontWeight: FontWeight.w400
   );
 
-
-  static final textWhite20W700 = GoogleFonts.playfairDisplay(
-    fontSize: 20,
-    color: Color(0xFFFFFFFF),
-    fontWeight: FontWeight.w700
-  );
-
-  static final timerParpal16W700 = GoogleFonts.dmSans(
-    fontSize: 16,
-    color: Color(0xFF764CA5),
-    fontWeight: FontWeight.w700
-  );
-
-  static final counterWhite20W700 = GoogleFonts.dmSans(
-    fontSize: 20,
-    color: Color(0xFFFFFFFF),
-    fontWeight: FontWeight.w700
-  );
 
 
 
