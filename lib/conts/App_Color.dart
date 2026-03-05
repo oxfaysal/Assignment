1607b0c59f08d94a298a2d735ecf54ab0fcd6b40
Faysal <0xfaysal64@gmail.com>
2026-03-05 22:16:00 +0600

    add image and title

---
+++ lib/conts/App_Color.dart
@@ -0,0 +1,21 @@
+import 'dart:ui';
+
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTextStyle {
+
+  static final titleBlack25 = GoogleFonts.roboto(
+      fontSize: 25,
+      color: Color(0xFF3C2F2F),
+      fontWeight: FontWeight.w600
+  );
+
+  static final titleBlack16 = GoogleFonts.roboto(
+      fontSize: 16,
+      color: Color(0xFF6A6A6A),
+      fontWeight: FontWeight.w400
+  );
+
+
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:assignment/conts/App_Color.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Practice",
+      title: "Food UI",
       home: HomePage(),
       debugShowCheckedModeBanner: false,
     );
@@ -23,22 +24,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(icon: Icon(Icons.arrow_back), onPressed: () {}),
-        actions: [
-          IconButton(icon: Icon(Icons.search), onPressed: () {}),
-        ],
+        actions: [IconButton(icon: Icon(Icons.search), onPressed: () {})],
         elevation: 0,
         backgroundColor: Colors.transparent,
         systemOverlayStyle: SystemUiOverlayStyle.light,
-
-
       ),
-      body: SizedBox(),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 350,
+              width: double.infinity,
+              child: Image.asset("assets/images/food.png"),
+            ),
+
+            SizedBox(height: 50),
+
+            Text(
+              "Cheeseburger Wendy's Burger",
+              style: AppTextStyle.titleBlack25,
+            ),
+            SizedBox(height: 15,),
+            Text(
+              "The Cheeseburger Wendy's Burger is a classic fast food burger that packs a punch of flavor in every bite. Made with a juicy beef patty cooked to perfection, it's topped with melted American cheese, crispy lettuce, ripe tomato, and crunchy pickles.",
+              style: AppTextStyle.titleBlack16,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
