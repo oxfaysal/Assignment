2fcc8b361b0d228b41968bb1a3639254679835f8
Faysal <0xfaysal64@gmail.com>
2026-03-05 21:50:16 +0600

    food app details page create

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 void main() {
+  runApp(MyApp());
+}
 
-  // Assignment: Working with Sets in Dart
-
-  Set<int> A = {1, 2, 3, 4, 5};
-  Set<int> B = {4, 5, 6, 7, 8};
-
-  for(int result in A.union(B)){
-    print(result);
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Practice",
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
   }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(icon: Icon(Icons.arrow_back), onPressed: () {}),
+        actions: [
+          IconButton(icon: Icon(Icons.search), onPressed: () {}),
+        ],
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        systemOverlayStyle: SystemUiOverlayStyle.light,
 
 
+      ),
+      body: SizedBox(),
+    );
+  }
 }
